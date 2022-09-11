84d55510ba28c0139fbe26d37542687e233866e6
riskerGoldClass <roakfwk30@naver.com>
2022-09-11 22:03:28 +0800

    add : jdbc 연결, 설정 추가

--- src/main/java/HWproject/firstspring/SpringConfig.java
+++ src/main/java/HWproject/firstspring/SpringConfig.java
@@ -1,14 +1,25 @@
 package HWproject.firstspring;
 
 
+import HWproject.firstspring.repository.JdbcMemberRepository;
 import HWproject.firstspring.repository.MemberRepository;
 import HWproject.firstspring.repository.MemoryMemberRepository;
 import HWproject.firstspring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -17,7 +28,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 
 }
