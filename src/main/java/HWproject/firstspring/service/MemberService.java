85d5a1dd48dd198f1216176920def4f34c311ac7
riskerGoldClass <roakfwk30@naver.com>
2022-09-12 01:17:39 +0800

    add : AOP가 필요한 상황(모든 핵심 로직의 실행 시간 측정)

--- src/main/java/HWproject/firstspring/service/MemberService.java
+++ src/main/java/HWproject/firstspring/service/MemberService.java
@@ -3,6 +3,7 @@ package HWproject.firstspring.service;
 import HWproject.firstspring.domain.Member;
 import HWproject.firstspring.repository.MemberRepository;
 import HWproject.firstspring.repository.MemoryMemberRepository;
+import org.hibernate.sql.AliasGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,10 +24,21 @@ public class MemberService {
      * 화원 가입
      */
     public long join(Member member){
-        //동명이인 회원 중복 X
-        validateDuplicateMember(member); //중복회원 검증
-        memberRepository.save(member);
-        return member.getId();
+
+        long start = System.currentTimeMillis();
+
+        try {
+            //동명이인 회원 중복 X
+            validateDuplicateMember(member); //중복회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        }finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = " + timeMs + "ms");
+        }
+
+
     }
 
     private void validateDuplicateMember(Member member) {
@@ -40,7 +52,17 @@ public class MemberService {
      *  전체 회원 조회
      */
     public List<Member> findMembers(){
-        return memberRepository.findAll();
+        long start = System.currentTimeMillis();
+
+        try{
+            return memberRepository.findAll();
+        }finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers = " + timeMs + "ms");
+        }
+
+
     }
 
     public Optional<Member> findOne(Long memberId){
